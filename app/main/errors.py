15538e4cc3e23fe58021e64aa7e4319f0e42b1ca
Joao Muller <muller.joaovitor00@gmail.com>
2018-09-02 13:56:33 -0300

    Update: Errors

--- app/main/errors.py
+++ app/main/errors.py
@@ -7,5 +7,4 @@ from . import main
 
 @main.app_errorhandler(404)
 def page_not_found(e):
-    return '<center>Página não encontrada!</br>' \ 
-           '%s</center>' %(e)
+    return '<center>Página não encontrada!</br>%s</center>' %(e)
